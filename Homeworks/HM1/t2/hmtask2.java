54890d5b601718bfb7a9245e5a014670c24b300e
Mariya <marisha_1793@mail.ru>
2023-04-21 10:41:11 +0700

    t2 исправлено

--- Homeworks/HM1/t2/hmtask2.java
+++ Homeworks/HM1/t2/hmtask2.java
@@ -4,16 +4,15 @@ package Homeworks.HM1.t2;
 
 public class hmtask2 {
     public static void main(String[] args) {
-        System.out.println("1 ");
+        System.out.print("1 ");
         for (int number = 2; number <= 1000; number++) {
             for (int count = 2; count <= number; count++) {
-                if (number % count == 0 & count == number) {
-                    System.out.printf("%d ", number);
-                } if (number % count == 0 & count != number) {
+                if (number % count == 0 & count != number) {
                     break;
+                } if (number % count == 0 & count == number) {
+                    System.out.printf("%d ", number);
                 }
             }
         }
-
     }
 }
