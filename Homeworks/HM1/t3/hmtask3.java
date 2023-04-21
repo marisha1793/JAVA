3515bc539cbadd1b6ab308dd4229db3a63a926ee
Mariya <marisha_1793@mail.ru>
2023-04-21 10:41:43 +0700

    t3 исправлено

--- Homeworks/HM1/t3/hmtask3.java
+++ Homeworks/HM1/t3/hmtask3.java
@@ -7,12 +7,26 @@ import java.util.Scanner;
 public class hmtask3 {
     public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Найдем сумму, введите первое число: ");
-        String number1 = iScanner.nextLine();
+        System.out.printf("Введите первое число: ");
+        double number1 = iScanner.nextDouble();
+        System.out.printf("Введите оператор (+,-,*,/): ");
+        char operator = iScanner.next().charAt(0);
         System.out.printf("Введите второе число: ");
-        String number2 = iScanner.nextLine();
-        int sum = Integer.parseInt(number1) + Integer.parseInt(number2);
-        System.out.printf("Сумма равна %d", sum);
+        double number2 = iScanner.nextDouble();
+        double result = 0;
+        switch(operator) {
+            case '+': result = number1 + number2;
+                break;
+            case '-': result = number1 - number2;
+                break;
+            case '*': result = number1 * number2;
+                break;
+            case '/': result = number1 / number2;
+                break;
+            default: System.out.printf("Введите корректный оператор");
+                break;
+        }
+        System.out.printf("Результат равен %f", result);
         iScanner.close();
     }
 }
